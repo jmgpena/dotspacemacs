99b843fb1a50fb074509aca2686777c0cdeab0f6
Jorge Pena <jorge@pond5.com>
2015-09-21 14:04:18 +0100

    Add erc layer

--- init.el
+++ init.el
@@ -46,6 +46,7 @@ values."
      lua
      finance
      sql
+     erc
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -231,3 +232,22 @@ layers configuration. You are free to put any user code."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sql-connection-alist
+   (quote
+    (("pond5-dev"
+      (sql-product
+       (quote postgres))
+      (sql-user "scott")
+      (sql-database "pond93")
+      (sql-server "192.168.56.100"))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
