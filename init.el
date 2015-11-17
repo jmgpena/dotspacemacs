a2ef331318184004b112a48bc03691a26e7b1597
jmgpena <jorge@pond5.com>
2015-11-17 10:26:01 +0000

    Fix conflict in init.el custom section

--- init.el
+++ init.el
@@ -242,11 +242,8 @@ layers configuration. You are free to put any user code."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-<<<<<<< HEAD
  '(paradox-github-token t)
-=======
  '(magit-push-arguments (quote ("--set-upstream")))
->>>>>>> 509eb6c96c98f4da3fbc9830a834910b9109370e
  '(sql-connection-alist
    (quote
     (("pond5-dev"
